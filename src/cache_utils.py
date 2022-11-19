a4e6365f2550b4efe1fad88de6e9de6123d5eb38
Wok <woctezuma@users.noreply.github.com>
2022-11-19 19:04:10 +0100

    Add utils for file operations of cached data

---
+++ src/cache_utils.py
@@ -0,0 +1,22 @@
+from src.json_utils import safe_load_json, safe_save_json
+
+DATA_FOLDER = "data/"
+FREE_LICENSE_FNAME = f"{DATA_FOLDER}free_licenses.json"
+ACTIVATED_LICENSE_FNAME = f"{DATA_FOLDER}activated_licenses.json"
+TIMESTAMP_FNAME = f"{DATA_FOLDER}last_updated.json"
+
+
+def load_activated_licenses(fname=ACTIVATED_LICENSE_FNAME):
+    return sorted(safe_load_json(fname), key=int)
+
+
+def save_activated_licenses(license_ids, fname=ACTIVATED_LICENSE_FNAME):
+    return safe_save_json(sorted(license_ids, key=int), fname)
+
+
+def load_free_licenses(fname=FREE_LICENSE_FNAME):
+    return sorted(safe_load_json(fname), key=int)
+
+
+def save_free_licenses(license_ids, fname=FREE_LICENSE_FNAME):
+    return safe_save_json(sorted(license_ids, key=int), fname)
