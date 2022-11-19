609d4bff645a8ebc7b6d7c23660d330ca5df727f
Wok <woctezuma@users.noreply.github.com>
2022-11-19 19:01:43 +0100

    Add a safe option in JSON utils

--- src/json_utils.py
+++ src/json_utils.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 
 def load_json(fname):
@@ -6,6 +7,19 @@ def load_json(fname):
         return json.load(file)
 
 
+def safe_load_json(fname):
+    try:
+        license_ids = load_json(fname)
+    except FileNotFoundError:
+        license_ids = []
+    return license_ids
+
+
 def save_json(data, fname, indent=4, sort_keys=True):
     with open(fname, "w", encoding="utf8") as file:
         json.dump(data, file, indent=indent, sort_keys=sort_keys)
+
+
+def safe_save_json(data, fname, indent=4, sort_keys=True):
+    Path(fname).parent.mkdir(parents=False, exist_ok=True)
+    save_json(data, fname, indent, sort_keys)
