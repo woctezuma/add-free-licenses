f27c7e31c5241c70774b68841e4ae16566a0cbf4
Wok <woctezuma@users.noreply.github.com>
2022-11-21 23:34:47 +0100

    Add JSON filename to store cookies

--- src/cache_utils.py
+++ src/cache_utils.py
@@ -4,6 +4,7 @@ DATA_FOLDER = "data/"
 FREE_LICENSE_FNAME = f"{DATA_FOLDER}free_licenses.json"
 ACTIVATED_LICENSE_FNAME = f"{DATA_FOLDER}activated_licenses.json"
 TIMESTAMP_FNAME = f"{DATA_FOLDER}last_updated.json"
+COOKIE_FNAME = f"{DATA_FOLDER}cookies.json"
 
 
 def load_activated_licenses(fname=ACTIVATED_LICENSE_FNAME):
