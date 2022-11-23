43c3e534deb68f75270292a8de408bd614e7bd74
Wok <woctezuma@users.noreply.github.com>
2022-11-23 16:21:02 +0100

    Fix bug in asyncio loop with batches

--- src/batch_utils.py
+++ src/batch_utils.py
@@ -12,6 +12,7 @@ def batch_process(new_free_ids, num_batches=NUM_BATCHES):
     num_ids_per_batch = int(HOURLY_RATE_LIMIT / NUM_BATCHES)
 
     newly_activated_ids = []
+    loop = asyncio.get_event_loop()
 
     for batch_no in range(num_batches):
         id_batch = []
@@ -26,12 +27,12 @@ def batch_process(new_free_ids, num_batches=NUM_BATCHES):
                 f"Batch n°{batch_no + 1}/{num_batches}: {id_batch}",
             )
 
-            loop = asyncio.get_event_loop()
             loop.run_until_complete(addlicense(id_batch))
-            loop.close()
 
             save_last_updated_timestamp()
 
             newly_activated_ids += id_batch
+
+    loop.close()
 
     return newly_activated_ids
